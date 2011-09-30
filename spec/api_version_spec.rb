b4d06c9130943fd178b97e256596c37563ac5676
Franck Verrot <franck@verrot.fr>
2011-09-30 14:15:35 +0200

    Forgot 'let' was part of MiniTest 2.5.0

--- spec/api_version_spec.rb
+++ spec/api_version_spec.rb
@@ -4,42 +4,34 @@ require 'action_controller/test_case'
 ACTR = ActionController::TestRequest
 
 describe Biceps::ApiVersion do
-  before do
-    @object = Biceps::ApiVersion
-  end
+  let(:object) { Biceps::ApiVersion }
 
   describe "without any appropriate Accept header" do
-    before do
-      @request =  ACTR.new({'HTTP_ACCEPT' => 'text/javascript'})
-    end
+    let(:request) { ACTR.new({'HTTP_ACCEPT' => 'text/javascript'}) }
 
     it "should never match" do
-      refute @object.new(1).matches?(@request)
+      refute object.new(1).matches?(request)
     end
   end
 
   describe "with an appropriate Accept header" do
-    before do
-      @request = ACTR.new({'HTTP_ACCEPT' => 'application/json, application/vnd.biceps;ver=1'})
-    end
+    let(:request) { ACTR.new({'HTTP_ACCEPT' => 'application/json, application/vnd.biceps;ver=1'}) }
 
     it "should match v1" do
-      assert @object.new(1).matches?(@request)
+      assert object.new(1).matches?(request)
     end
 
     it "should not match v2" do
-      refute @object.new(2).matches?(@request)
+      refute object.new(2).matches?(request)
     end
   end
 
   describe "with an inappropriate application name" do
     # Even Chuck Norris can't access the api if the application name is inappropriate
-    before do
-      @request = ACTR.new('HTTP_ACCEPT' => 'application/json, application/vnd.chucknorris;ver=1')
-    end
+    let(:request) { ACTR.new('HTTP_ACCEPT' => 'application/json, application/vnd.chucknorris;ver=1') }
 
     it "should never match" do
-      refute @object.new(1).matches?(@request)
+      refute object.new(1).matches?(request)
     end
   end
 end
