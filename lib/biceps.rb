e9bd3b0d2d49403affc0d815102ffea11a00b68e
Damien Mathieu <42@dmathieu.com>
2011-10-13 16:53:11 +0200

    add our internal versioned model serializer

--- lib/biceps.rb
+++ lib/biceps.rb
@@ -3,4 +3,5 @@ require 'biceps/core_ext/action_dispatch/routing/mapper'
 module Biceps
   autoload :ApiVersion, 'biceps/api_version'
   autoload :Jsonp,      'biceps/jsonp'
+  autoload :Serializer,      'biceps/serializer'
 end
