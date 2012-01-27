274b46635b7083474459de0e875069233f53cd9f
Damien Mathieu <42@dmathieu.com>
2012-01-27 11:56:35 +0100

    Too bad, I kinda liked autoload
    http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-core/41149

--- lib/biceps.rb
+++ lib/biceps.rb
@@ -1,12 +1,12 @@
 require 'biceps/core_ext/action_dispatch/routing/mapper'
 require 'biceps/core_ext/action_controller/base'
 
-module Biceps
-  autoload :ApiVersion,      'biceps/api_version'
-  autoload :ModelVersioning, 'biceps/model_versioning'
-  autoload :Jsonp,           'biceps/jsonp'
-  autoload :Serializer,      'biceps/serializer'
-  autoload :Parser,          'biceps/parser'
-  autoload :Builder,         'biceps/builder'
-  autoload :TestHelper,      'biceps/test_helper'
-end
+module Biceps; end
+
+require 'biceps/api_version'
+require 'biceps/model_versioning'
+require 'biceps/jsonp'
+require 'biceps/serializer'
+require 'biceps/parser'
+require 'biceps/builder'
+require 'biceps/test_helper'
