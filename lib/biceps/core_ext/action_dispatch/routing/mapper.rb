baee9819036259cf2e6f2a8a229e2cef859a137f
Damien Mathieu <damien.mathieu@shazino.com>
2012-02-29 17:22:16 +0100

    fix multiple versions with module

--- lib/biceps/core_ext/action_dispatch/routing/mapper.rb
+++ lib/biceps/core_ext/action_dispatch/routing/mapper.rb
@@ -6,16 +6,22 @@ require 'action_controller'
 module ActionDispatch::Routing
   class Mapper
 
-    def api_version(version, opts={})
-      if opts[:module] && opts[:module].empty?
-        scope_module = nil
-      else
-        scope_module = opts[:module] || "v#{version}"
-      end
+    def api_version(versions, opts={})
+      Array(versions).each do |version|
+        if opts[:module] && opts[:module].empty?
+          scope_module = nil
+        elsif opts[:module]
+          scope_module = opts[:module]
+        elsif !version.nil?
+          scope_module = "v#{version}"
+        else
+          scope_module = nil
+        end
 
-      scope :module => scope_module do
-        constraints Biceps::ApiVersion.new(version) do
-          yield if block_given?
+        scope :module => scope_module do
+          constraints Biceps::ApiVersion.new(version) do
+            yield if block_given?
+          end
         end
       end
     end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -12,6 +12,10 @@ module Bar
     def foo_bar
       render :text => 'test with bar module'
     end
+
+    def foo_double
+      render :text => 'test with double and module'
+    end
   end
 end
 
@@ -28,6 +32,10 @@ class Biceps::IntegrationTest < ActionDispatch::IntegrationTest
         get '/test' => lambda { |env| [200,{},['test v2']] }
       end
 
+      api_version [1, nil] do
+        get '/double' => lambda { |env| [200, {}, ['test double']] }
+      end
+
       get '/test'   => lambda { |env| [200,{},['test']] }
 
 
@@ -36,6 +44,10 @@ class Biceps::IntegrationTest < ActionDispatch::IntegrationTest
       end
       api_version 1, :module => 'bar' do
         get '/foo_bar' => 'api#foo_bar'
+      end
+
+      api_version [1, 2] do
+        get '/foo_double' => 'api#foo_double'
       end
 
     end
@@ -51,6 +63,17 @@ class Biceps::IntegrationTest < ActionDispatch::IntegrationTest
 
     get '/test', {}, { 'HTTP_ACCEPT' => "application/vnd.biceps;ver=2" }
     assert_equal 'test v2', @response.body
+  end
+
+  test "routing with double versions" do
+    get '/double'
+    assert_equal 'test double', @response.body
+
+    get '/double', {}, { 'HTTP_ACCEPT' => "application/vnd.biceps;ver=1" }
+    assert_equal 'test double', @response.body
+
+    get '/double', {}, { 'HTTP_ACCEPT' => "application/vnd.biceps;ver=2" }
+    assert_equal 404, @response.status
   end
 
   test "routing with specified module" do
