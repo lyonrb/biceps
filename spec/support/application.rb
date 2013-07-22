90a541c69e08742381c50e00bd2a238c4b72a67d
Damien Mathieu <42@dmathieu.com>
2013-07-22 17:08:15 +0200

    update to nicely run with rails 4

--- spec/support/application.rb
+++ spec/support/application.rb
@@ -1,6 +1,6 @@
 module Biceps
   class Application < Rails::Application
-    config.secret_token = 'x' * 30
+    config.secret_key_base = 'x' * 30
   end
 end
 Rails.logger = Logger.new('/dev/null')
